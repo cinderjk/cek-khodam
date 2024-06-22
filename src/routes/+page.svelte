d2eca4f1740af59e5c1cf040a00fc6675b02b89c
cinderjk <cinderjk@gmail.com>
2024-06-22 09:01:51 +0700

    WIP: Add new khodam

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
                     CEK KHODAM ONLINE
             </a>
             <div class="w-full bg-[rgba(255,255,255,0.2)] rounded-lg shadow md:mt-0 sm:max-w-lg xl:p-0 dark:bg-gray-800">
-                <div class="p-6 space-y-4 md:space-y-6 lg:space-y-8 sm:p-8">
+                <div class="p-4 space-y-4 md:space-y-6 lg:space-y-8 sm:p-8">
                     <h1
                         class="text-lg font-bold leading-tight tracking-tight text-center text-white md:text-xl dark:text-white">
                         CEK KHODAM YANG SELALU MENEMANIMU
@@ -43,24 +43,24 @@
         </div>
     {:else}
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen pt:mt-0">
-            <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-lg xl:p-0 dark:bg-gray-800">
-                <div class="p-6 space-y-4 md:space-y-6 lg:space-y-4 sm:p-8">
+            <div class="w-full bg-[rgba(255,255,255,0.2)] rounded-lg shadow md:mt-0 sm:max-w-lg xl:p-0 dark:bg-gray-800">
+                <div class="p-4 space-y-4 md:space-y-6 lg:space-y-4 sm:p-8">
                     <h1
-                        class="text-md font-bold leading-tight tracking-tight text-center text-gray-900 md:text-lg dark:text-white">
-                        {checkedName}, KHODAM YANG SELALU MENEMANIMU ADALAH
+                        class="text-sm font-bold leading-tight tracking-tight text-center text-gray-300 md:text-lg dark:text-white">
+                        {checkedName}, KHODAM KAMU ADALAH
                     </h1>
-                    <div class="space-y-4 md:space-y-6">
+                    <div class="grid justify-items-center space-y-4 md:space-y-6">
+                        <img class="h-auto max-w-72 text-center" src="/khodam/{result_image}" alt="image description">
                         <h1
-                            class="text-lg font-bold leading-tight tracking-tight text-center text-gray-900 md:text-2xl dark:text-white">
+                            class="text-lg font-bold leading-tight tracking-tight text-center text-white md:text-2xl dark:text-white">
                             {result_name}
                         </h1>
-                        <img class="h-auto max-w-full" src="/khodam/{result_image}" alt="image description">
-
+                        
                         <button type="submit" on:click={uncheckKhodam} autofocus
                             class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-lg p-4 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                             Cek Lagi
                         </button>
-                        <p class="text-sm font-light text-center text-gray-500 dark:text-gray-300">
+                        <p class="text-sm font-light text-center text-gray-100 dark:text-gray-300">
                             Dibuat oleh
                             <a href="https://github.com/cinderjk" class="font-medium text-primary-600 hover:underline dark:text-primary-500" target="_blank">
                                 Fahri
@@ -111,11 +111,77 @@
             {
                 "name": "Samurai Api",
                 "image": "samurai_api.jpg"
-            }
+            },
+            {
+                "name": "Naga Bear Brand",
+                "image": "naga_bear_brand.jpeg"
+            },
+            {
+                "name": "Buaya Darat",
+                "image": "buaya_darat.jpeg"
+            },
+            {
+                "name": "Buaya Darat",
+                "image": "buaya_darat.jpeg"
+            },
+            {
+                "name": "Tuyul Botak",
+                "image": "tuyul_botak.jpeg"
+            },
+            {
+                "name": "Tolak Angin",
+                "image": "tolak_angin.jpg"
+            },
+            {
+                "name": "Gardu PLN",
+                "image": "gardu_pln.jpg"
+            },
+            {
+                "name": "Badut Mixue",
+                "image": "badut_mixue.jpeg"
+            },
+            {
+                "name": "Lohan Arab",
+                "image": "lohan_arab.jpeg"
+            },
+            {
+                "name": "Barongsai",
+                "image": "barongsai.jpeg"
+            },
+            {
+                "name": "Rangda",
+                "image": "rangda.png"
+            },
+            {
+                "name": "Ambatukam",
+                "image": "ambatukam.png"
+            },
+            {
+                "name": "Dugong",
+                "image": "dugong.png"
+            },
+            {
+                "name": "Laptop Rusak",
+                "image": "laptop_rusak.png"
+            },
+            {
+                "name": "Beat Lipat",
+                "image": "beat_lipat.png"
+            },
         ]
 
         const result = khodams[Math.floor(Math.random() * khodams.length)];
-        result_name = result.name;
+
+        if(name == 'fahri' || name == 'fahri pano' || name == 'khoiri') {
+            result.name = 'Greesella Sophina Adhalia ❤';
+            result.image = 'cintakuu.jpg';
+        }
+
+        if(name == 'lensa' || name == 'ellen' || name == 'lensa ellentika') {
+            result.name = 'Choi Woo-shik ❤';
+            result.image = 'cws.png';
+        }
+        result_name = result.name.toUpperCase();
         result_image = result.image;
 
         error = '';
